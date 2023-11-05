16ac96c954bc26855281181c50227fca4e7e975b
SeniboDonPedro <senibodonpedro@gmail.com>
2023-11-05 09:53:21 +0100

    completed app. improve upon later

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "./SearchBar.css"
 
 const SearchBar = ({ onSubmit }) => {
   const [term, setTerm] = useState("")
@@ -14,10 +15,10 @@ const SearchBar = ({ onSubmit }) => {
   }
 
   return (
-    <div>
+    <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
+        <label htmlFor="">Enter Search Term</label>
         <input value={term} onChange={handleChange} />
-        <button>Submit</button>
       </form>
     </div>
   )
